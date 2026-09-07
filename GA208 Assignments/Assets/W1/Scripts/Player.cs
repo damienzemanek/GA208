2c83c9f43669c51d9f80cbd11cd694f8c4366c24
Emil Z <damienzemanek@lcad.edu>
2026-09-06 18:08:04 -0700

    Done

--- GA208 Assignments/Assets/W1/Scripts/Player.cs
+++ GA208 Assignments/Assets/W1/Scripts/Player.cs
@@ -1,14 +1,51 @@
 using System;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Vector2 moveDir;
-    bool isMoving = false;
-    public float speed = 10.0f;
-    
-    
+    [ShowInInspector, ReadOnly] Vector2 moveDir;
+    [ShowInInspector, ReadOnly] bool isMoving = false;
+    [ShowInInspector, ReadOnly] int currentSeeds;
+    [ShowInInspector, ReadOnly] int plantedSeeds = 0;
+    [Required] public GameObject seedPrefab;
+    [Required] public TextMeshProUGUI txt_remainingSeedsNum;
+    [Required] public TextMeshProUGUI txt_plantedSeedsNum;
+    public int maxSeeds = 5;
+    public float speed = 0.012f;
+
+
+    void OnEnable()
+    {
+        currentSeeds = maxSeeds;
+        UpdateUI();
+    }
+
     void FixedUpdate()
+    {
+        if(!isMoving) return;
+        transform.Translate(moveDir * speed);
+    }
+
+    void Update()
+    {
+        PollMove();
+        PollSpawnSeed();
+    }
+    
+    
+    void PollSpawnSeed()
+    {
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        if (currentSeeds <= 0) return;
+        currentSeeds--;
+        plantedSeeds++;
+        Instantiate(seedPrefab, transform.position, Quaternion.identity);
+        UpdateUI();
+    }
+
+    void PollMove()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
         {
@@ -17,12 +54,11 @@ public class Player : MonoBehaviour
         }
         else 
             isMoving = false;
-
     }
 
-    void Update()
+    void UpdateUI()
     {
-        if(!isMoving) return;
-        transform.Translate(moveDir * speed);
+        txt_remainingSeedsNum.text = currentSeeds.ToString();
+        txt_plantedSeedsNum.text = plantedSeeds.ToString();
     }
 }

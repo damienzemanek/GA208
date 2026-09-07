e8be173dcdce38014c11017f05c076515fdadffd
Emil Z <damienzemanek@lcad.edu>
2026-09-06 17:51:25 -0700

    Odin

--- GA208 Assignments/Assets/W1/Scripts/Player.cs
+++ GA208 Assignments/Assets/W1/Scripts/Player.cs
@@ -1,6 +1,28 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector2 moveDir;
+    bool isMoving = false;
+    public float speed = 10.0f;
     
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            isMoving = true;
+            moveDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+        else 
+            isMoving = false;
+
+    }
+
+    void Update()
+    {
+        if(!isMoving) return;
+        transform.Translate(moveDir * speed);
+    }
 }
